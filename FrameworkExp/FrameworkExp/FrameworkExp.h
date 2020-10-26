4855e1d2727dbbb8a6844c518d418959bbe5c48a
Nuno Domingues <nunogdomingues@gmail.com>
2020-10-26 16:50:55 +0000

    initial commit

---
+++ FrameworkExp/FrameworkExp/FrameworkExp.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkExp.h
+//  FrameworkExp
+//
+//  Created by Nuno Domingues on 26/10/2020.
+//  Copyright © 2020 Nuno Domingues. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkExp.
+FOUNDATION_EXPORT double FrameworkExpVersionNumber;
+
+//! Project version string for FrameworkExp.
+FOUNDATION_EXPORT const unsigned char FrameworkExpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkExp/PublicHeader.h>
+
+
